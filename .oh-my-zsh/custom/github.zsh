9f0594640e331705051e4453d6f9c147f339c2be
Liam Graham <liam.graham@chde.qld.gov.au>
2022-11-10 09:23:55 +1000

    Update github.zsh

--- .oh-my-zsh/custom/github.zsh
+++ .oh-my-zsh/custom/github.zsh
@@ -5,28 +5,7 @@ clone-me() {
     then
         gh repo clone $GITHUB_USERNAME/$1
     else
-        echo "Usage: gh-me [repo-name]"
+        echo "Usage: clone-me [repo-name]"
     fi
 }
 
-clone-work() {
-    if [ -n "$1" ]
-    then
-        gh repo clone myqld/$1
-    else
-        echo "Usage: gh-me [repo-name]"
-    fi
-}
-
-repo() {
-    if [ -n "$1" ]
-    then
-        firefox "https://github.com/myqld/$1" 
-    else
-        REPO_NAME=$(basename `pwd`)
-        firefox "https://github.com/myqld/$REPO_NAME"
-    fi
-}
-
-alias gistl='gh gist list'
-alias gistc='gh gist clone' 
