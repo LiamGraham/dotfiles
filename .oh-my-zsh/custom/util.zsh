04f72bc4c151121bc5b534f1fcc9a246fe831550
Liam Graham <liam.graham@maxkelsen.com>
2023-05-19 11:51:19 +1000

    Update .oh-my-zsh/custom files

--- .oh-my-zsh/custom/util.zsh
+++ .oh-my-zsh/custom/util.zsh
@@ -1,4 +1,5 @@
-export REPOS_DIR="$HOME/work/repos"
+export REPOS_DIR="$HOME/work"
+export GITHUB_USERNAME="liamgraham"
 
 cdiff() {
   if [[ $# -eq 2 ]]; then
@@ -15,7 +16,7 @@ coverage() {
     project_dir=$(readlink -f "$1")
   else
     echo "Usage: coverage [ROOT_DIR]"
-    exit 1
+    return 1
   fi
   
   if [[ $? -eq 0 ]]; then
@@ -40,5 +41,48 @@ cdroot() {
   if [[ $? -eq 0 ]]; then
     . cd "$project_dir"
   fi
+}
+
+clone-work() {
+  if [ -n "$1" ]
+  then
+    git clone git@github.com:MaxKelsen/$1.git
+  else
+    echo "Usage: clone-work [repo-name]"
+  fi
+}
+
+clone-me() {
+  if [ -z "$1" ]; then
+    echo "Usage: clone-me [repo-name]"
+    return 1
+  fi
+
+  gh repo clone $GITHUB_USERNAME/$1
+}
+
+cdprop() {
+  target_name="core-api"
+  if [ $1 ]; then
+    target_name=$1
+  fi
+
+  cd $REPOS_DIR/mk-propel-$target_name
+}
+
+granch() {
+  if [ -z "$1" ]
+  then
+    echo "Usage: granch PATTERN"
+    return 1
+  fi
+
+  match=$(git branch --format "%(refname:short)" | grep -m 1 "$1")
+  if [ -z $match ]; then
+    rich -p "[red]! No matching branches found"
+    return 1
+  fi
+
+  git checkout $match
 }
 
